bfabbe0fecd45c299cbdcaf7804fa83b731c7ae9
s.navidi <shnavidi18@gmail.com>
2024-08-10 00:58:29 +0330

    add  react-icons and swiper library,add some config to eslint and tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '0',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1038px',
+    },
     extend: {
       fontFamily: {
         sans: ['iranSans'],
+      },
+      colors: {
+        primary: {
+          100: '#a5ffce',
+          200: '#4ae290',
+          300: '25c870',
+        },
+        neutral: {
+          100: '#ffffff',
+          200: '#dedee3',
+          300: '#9797a1',
+          400: '#595962',
+          500: '#131316',
+        },
+        page: '#fcfcff',
+      },
+      backgroundImage: {
+        banner: "url('/src/assets/images/home/banner/bg.png')",
+        faq: "url('/src/assets/images/home/faq/bg.svg')",
+      },
+      backgroundSize: {
+        auto: 'auto',
+        cover: 'cover',
+        contain: 'contain',
+        custom: '95%',
       },
     },
   },
