72477676d01051e11f5c16f0b58c75fa2dc0d249
s.navidi <shnavidi18@gmail.com>
2024-08-11 04:06:37 +0330

    add some package like axios,react-hot-toast ,react query  and wrap app with them,create an instance from axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
